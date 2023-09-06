6ae4d820881e930764f46a1991d1e02b88602a1a
Lylio ahb <lylioarhab@gmail.com>
2023-09-06 19:48:19 +0200

    Back-end changes, broke website

--- myportfolio/myportfolio/urls.py
+++ myportfolio/myportfolio/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+
 urlpatterns = [
     path("", include("pflylio.urls")),
     path("admin/", admin.site.urls),
+   
 ]
